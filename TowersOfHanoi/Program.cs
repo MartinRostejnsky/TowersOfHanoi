10274ffa3aee5c08e15dd0d9bc49690eec24a133
Martin <martin.rostejnsky@email.cz>
2023-01-22 20:26:49 +0100

    odd

--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -1,15 +1,12 @@
 ﻿using TowersOfHanoi;
 using System;
 
-int discs = 6;
+int discs = 45;
 
 Game toh = new Game(discs);
 
-if (discs == 1)
-{
-    try { toh.Move(0, 2); } catch (Exception ex) { Console.WriteLine(ex); }
-    Console.WriteLine(toh.ToString());
-}else if (discs % 2 == 0) {
+
+if (discs % 2 == 0) {
     while (!(toh.Towers[2].Count() == discs))
     {
         try { toh.Move(0, 1); } catch (Exception ex) { Console.WriteLine(ex); }
@@ -18,14 +15,20 @@ if (discs == 1)
         Console.WriteLine(toh.ToString());
         try { toh.Move(1, 2); } catch (Exception ex) { Console.WriteLine(ex); }
         Console.WriteLine(toh.ToString());
-        Console.ReadLine();
     }
 }
 else
 {
+    try { toh.Move(0, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+    Console.WriteLine(toh.ToString());
     while (!(toh.Towers[0].Count() == 0 & toh.Towers[2].Count() == discs))
     {
-
+        try { toh.Move(0, 1); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
+        try { toh.Move(1, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
+        try { toh.Move(0, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
     }
 
 }
