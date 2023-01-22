a2173080496c824bbd96d312e1cd418ca9656cc1
Martin <martin.rostejnsky@email.cz>
2023-01-22 20:15:39 +0100

    even

---
+++ TowersOfHanoi/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+    class Game
+    {
+        public Stack<int>[] Towers = {
+        new Stack<int>(),
+        new Stack<int>(),
+        new Stack<int>()
+    };
+
+
+        public Game (int discs)
+        {
+            for (int i = discs; i > 0; i--)
+            {
+                Towers[0].Push(i);
+            }
+        }
+
+        public void Move (int a, int b) {
+            int aval;
+            int bval;
+            if (a == b | a > 2 | b > 2 | (!Towers[a].TryPeek(out aval) & !Towers[b].TryPeek(out bval)))
+            {
+                throw new Exception("Impossible move!");
+            }
+            if (!Towers[a].TryPeek(out aval))
+            {
+                this.Towers[a].Push(this.Towers[b].Pop());
+            }
+            else if (!Towers[b].TryPeek(out bval))
+            {
+                this.Towers[b].Push(this.Towers[a].Pop());
+            }
+            else if (aval > bval)
+            {
+                this.Towers[a].Push(this.Towers[b].Pop());
+            }
+            else
+            {
+                this.Towers[b].Push(this.Towers[a].Pop());
+            }
+        }
+
+        public string ToString()
+        {
+            string state = "";
+            foreach (Stack<int> i in Towers)
+            {
+                state += string.Join("", i);
+                state += " | ";
+            }
+            return state;
+        } 
+
+    }
+}
--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TowersOfHanoi;
+using System;
+
+int discs = 6;
+
+Game toh = new Game(discs);
+
+if (discs == 1)
+{
+    try { toh.Move(0, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+    Console.WriteLine(toh.ToString());
+}else if (discs % 2 == 0) {
+    while (!(toh.Towers[2].Count() == discs))
+    {
+        try { toh.Move(0, 1); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
+        try { toh.Move(0, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
+        try { toh.Move(1, 2); } catch (Exception ex) { Console.WriteLine(ex); }
+        Console.WriteLine(toh.ToString());
+        Console.ReadLine();
+    }
+}
+else
+{
+    while (!(toh.Towers[0].Count() == 0 & toh.Towers[2].Count() == discs))
+    {
+
+    }
+
+}
